b738125b8a35741cf7749bf656ffb233fac3c90a
Diego_Rus <diegorus.1992@gmail.com>
2023-10-20 14:32:08 -0300

    implemented Adding, update and delete book functions

--- src/controller.js
+++ src/controller.js
@@ -12,6 +12,30 @@ class BooksController{
         const [result] = await poolRequest.query(`SELECT * from books WHERE id=(?)`, [book.id]);  
         res.json(result);
     }
+
+    async insert(req, res){
+        const book = req.body;
+        const [result] = await poolRequest.query(
+                `INSERT INTO books(name, author, category, publicationDate, isbn) VALUES(?, ?, ?, ?, ?)`, 
+                [book.name, book.author, book.category, book.publicationDate, book.isbn]
+            );
+        res.json({"Book added: ": result.insertId});
+    }
+
+    async update(req, res){
+        const book = req.body;
+        const [result] = await poolRequest.query(
+            `UPDATE books SET name = ?, author = ?, category = ?, publicationDate = ?, isbn = ? WHERE id = ?`,
+            [book.name, book.author, book.category, book.publicationDate, book.isbn, book.id]
+        );
+        res.json({"Book updated" : result.affectedRows});
+    }
+
+    async deleteByIsbn(req, res){
+        const book = req.body;
+        const [result] = await poolRequest.query(`DELETE FROM books WHERE isbn = ?`, [book.isbn]);
+        res.json({"Book deleted" : result.affectedRows});
+    }
 }
 
 export const books = new BooksController();
--- src/routes.js
+++ src/routes.js
@@ -5,3 +5,6 @@ export const router = Router();
 
 router.get('/books', books.getAll);
 router.get('/book', books.getOne);
+router.post('/book', books.insert);
+router.put("/book", books.update);
+router.delete("/book", books.deleteByIsbn);
